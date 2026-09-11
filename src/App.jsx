28bb0f7e7aa50ae5bf84f16990e83399b8fb214a
Hansaka.KA <hansaka.koda@gmail.com>
2026-09-11 11:01:26 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,88 @@
-import { useState } from "react";
-import heroImg from "./assets/hero.png";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import "./App.css";
+import { useState, useEffect, use } from "react";
+import Search from "./components/Search";
+import Spinner from "./components/Spinner";
 
-const Card = ({ title }) => {
+const API_BASE_URL = "https://api.themoviedb.org/3";
 
-  const [hasLiked, setHasLiked] = useState(false);
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
-  return (
-    <div className="card">
-      <h2> {title} </h2>
-
-    <button onClick={() => setHasLiked(!hasLiked)}>
-      {hasLiked ? "❤️" : "🤍"}
-    </button>
-
-    </div>
-  );
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
 };
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage("");
+
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+
+      if (data.response === "False") {
+        setErrorMessage(data.error);
+        setMovies([]);
+      }
+
+      setMovies(data.results || []);
+    } catch (error) {
+      setErrorMessage("Failed to fetch movies. Please try again later.");
+      console.error("Error fetching movies:", error);
+      setMovies([]);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return (
-    <div className="card-container">
-       <Card title="Star wars" />
-      <Card title="Avatar" />
-      <Card title="Titanic" />
-    </div>
+    <main>
+      <div className="pattern"></div>
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="hero" className="hero" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            without the Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+
+        <section className="all-movies">
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="error-message">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movies.map((movie) => (
+                <p key={movie.id} className="text-white">
+                  {movie.title}
+                </p>
+              ))}
+            </ul>
+          )}
+        </section>
+      </div>
+    </main>
   );
 };
 
